65d1ba61804b9ec246ed094323d01ca7a9344a66
ldoughty2 <ldoughty2@huskers.unl.edu>
2024-12-04 17:18:28 -0600

    switched from digitalWrite() to ioport. Check your wiring!

--- src/servomotor.c
+++ src/servomotor.c
@@ -19,15 +19,14 @@
 #include "interrupt_support.h"
 
 #define SERVO_PIN           (22)
-#define PULSE_INCREMENT_uS  (500 * 1000) // pulse varies from 500 (fully left) to 2500 (fully right). 1500 is center
-#define SIGNAL_PERIOD_uS    (20000 * 1000)
-#define LEFTBUTTON          (1 << 2)
-#define RIGHTBUTTON         (1 << 3)
-#define LEFTSWITCH          (1 << 14)
+#define PULSE_INCREMENT_uS  (500) // pulse varies from 500 (fully left) to 2500 (fully right). 1500 is center
+#define SIGNAL_PERIOD_uS    (20000)
 static int volatile pulse_width_us;
+static cowpi_ioport_t volatile *ioport;
 static void handle_timer_interrupt();
 
 void initialize_servo() {
+    ioport = (cowpi_ioport_t *) (0xD0000000);
     cowpi_set_output_pins(1 << SERVO_PIN);
     center_servo();
     register_periodic_timer_ISR(0, PULSE_INCREMENT_uS, handle_timer_interrupt);
@@ -59,16 +58,24 @@ void rotate_full_counterclockwise() {
     pulse_width_us = 2500;
 }
 
+void writeToServo(int turn_on) {
+    if (turn_on) {
+        ioport->output |= (1 << SERVO_PIN);
+    } else {
+        ioport->output &= ~(1 << SERVO_PIN);
+    }
+}
+
 static void handle_timer_interrupt() {
-    static int time_until_next_rising_edge = 0;
-    static int time_until_next_falling_edge = 0;
+    static int time_until_next_rising_edge = PULSE_INCREMENT_uS;
+    static int time_until_next_falling_edge = PULSE_INCREMENT_uS;
     time_until_next_rising_edge -= PULSE_INCREMENT_uS ;
     time_until_next_falling_edge -= PULSE_INCREMENT_uS;
-    if (time_until_next_rising_edge <= 0){
-        digitalWrite(20, 1); // Start the pulse
+    if (time_until_next_rising_edge == 0){
+        writeToServo(1);
         time_until_next_rising_edge = SIGNAL_PERIOD_uS;
-        time_until_next_falling_edge = pulse_width_us * 1000;
-    } else if (time_until_next_falling_edge <= 0){
-        digitalWrite(20, 0); // Finish the pulse
+        time_until_next_falling_edge = pulse_width_us;
+    } else if (time_until_next_falling_edge == 0){
+        writeToServo(0);
     }
 }
