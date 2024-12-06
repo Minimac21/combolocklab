6ea86cbbd31d0a0bf9a6232cfb278b831989f030
ldoughty2 <ldoughty2@huskers.unl.edu>
2024-12-05 19:13:45 -0600

    added set_system_to_locked()

--- src/lock-controller.c
+++ src/lock-controller.c
@@ -20,7 +20,11 @@
 #include "rotary-encoder.h"
 #include "servomotor.h"
 
+#define LEFT_LED_PIN    (21)
+#define RIGHT_LED_PIN   (20)
+
 static uint8_t combination[3] __attribute__((section (".uninitialized_ram.")));
+
 
 uint8_t const *get_combination() {
     return combination;
@@ -31,8 +35,21 @@ void force_combination_reset() {
     combination[1] = 10;
     combination[2] = 15;
 }
+
+void set_system_to_locked(uint8_t *combination){
+    digitalWrite(LEFT_LED_PIN, 1);
+    digitalWrite(RIGHT_LED_PIN, 0);
+    rotate_full_clockwise();
+    display_string(1, "LOCKED");
+    
+    // SHOW THE COMBINATION (delete later?)
+    static char combo_buffer[22] = {0};
+    sprintf(combo_buffer, "Combo: %02d-%02d-%02d", combination[0], combination[1], combination[2]);
+    display_string(3, combo_buffer);
+}
+
 void initialize_lock_controller() {
-    ;
+    set_system_to_locked(combination);
 }
 
 void control_lock() {
